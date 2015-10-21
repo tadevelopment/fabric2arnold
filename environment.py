1dc529136651a5a178a6f3b7841d7aad61cab242
phtaylor <phil@hordesoftware.co>
2015-10-21 11:55:19 -0400

    adding the asynchtask submodule. added environment setup script.

---
+++ environment.py
@@ -0,0 +1,18 @@
+
+
+#
+# Copyright 2015 Horde Software Inc. All rights reserved.
+#
+
+import os
+import imp
+
+# Load in the HApp environment
+fabric2arnoldDir = os.path.dirname(os.path.realpath(__file__))
+
+# Add Horde KL extensions
+fabric2arnoldExtPaths = [os.path.join(fabric2arnoldDir, 'stage'), os.path.join(fabric2arnoldDir, 'asynctask', 'exts')]
+if 'FABRIC_EXTS_PATH' in os.environ:
+    fabric2arnoldExtPaths.append(os.environ['FABRIC_EXTS_PATH'])
+os.environ['FABRIC_EXTS_PATH'] = os.pathsep.join(fabric2arnoldExtPaths)
+
